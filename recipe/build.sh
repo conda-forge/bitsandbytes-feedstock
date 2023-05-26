cb1adb5344c1fd6ab18cbdec9cd7c2214ee4da6f
Uwe Korn <uwelk@xhochy.com>
2023-05-26 18:38:50 +0000

    Build with CUDA support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+set -exuo pipefail
+
 make GPP=$CXX cpuonly
+
+if [[ "${cuda_compiler_version}" = 10.* ]]; then
+  make NVCC="${CUDA_HOME}/bin/nvcc -ccbin=${CC}" GPP=$CXX CUDA_VERSION=${cuda_compiler_version/./} cuda110
+elif [[ "${cuda_compiler_version}" = 11.* ]]; then
+  make NVCC="${CUDA_HOME}/bin/nvcc -ccbin=${CC}" GPP=$CXX CUDA_VERSION=${cuda_compiler_version/./} cuda11x
+elif [[ "${cuda_compiler_version}" = 12.* ]]; then
+  make NVCC="${CUDA_HOME}/bin/nvcc -ccbin=${CC}" GPP=$CXX CUDA_VERSION=${cuda_compiler_version/./} cuda12x
+fi
+
 python setup.py install
